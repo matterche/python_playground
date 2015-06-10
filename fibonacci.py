9d7de7f1c9f90f91d205c3fd566f068369f47b1a
Matthias Erche <matthias.erche@epost-dev.de>
2015-06-10 11:28:02 +0200

    Add recursive fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -13,8 +13,23 @@ def fibonacci():
     return
 
 
-fibu_expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+def fibonacci_recursive(actual=[0, 1], f1=0, f2=1, max_result=7000):
+    result = f1 + f2
 
-generator = fibonacci()
+    if result > max_result:
+        return actual
+
+    actual.append(result)
+    return actual + fibonacci_recursive(actual, f2, result)
+
+
+fibonacci_expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+
+fibonacci_generator = fibonacci()
+fibonacci_rec = fibonacci_recursive()
+
+print fibonacci_rec
+
 for i in range(21):
-    assert fibu_expected[i] == generator.next()
+    assert fibonacci_expected[i] == fibonacci_generator.next()
+    assert fibonacci_expected[i] == fibonacci_rec[i]

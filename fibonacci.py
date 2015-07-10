d580e74c3e3a597535a813a43de546a418bc5fc8
Matthias Erche <matthias.erche@epost-dev.de>
2015-07-10 14:43:07 +0200

    Optimize comprehension with return condition

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,7 @@
-def fibonacci():
+def fibonacci(max_results):
     f1 = -1
     f2 = 0
-    while True:
+    for x in range(max_results):
         if f1 == -1:
             f1 = 1
             yield 0
@@ -25,7 +25,7 @@ def fibonacci_recursive(actual=[0, 1], f1=0, f2=1, max_result=7000):
 
 fibonacci_expected = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
 
-fibonacci_generator = fibonacci()
+fibonacci_generator = fibonacci(20)
 fibonacci_rec = fibonacci_recursive()
 
 print fibonacci_rec

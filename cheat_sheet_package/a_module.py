16e1e72d53961f0650a015beb07bc0382747a784
Matthias Erche <matthias.erche@epost-dev.de>
2015-07-10 14:40:18 +0200

    Add example package with module

---
+++ cheat_sheet_package/a_module.py
@@ -0,0 +1,9 @@
+# a Python file in a package is a module
+
+# from xyz import * should not be used to prevent importing stuff you
+
+# import of packages allows alias definition
+
+# import triggers execution of toplevel code
+
+# package initialization code in __init__.py is always executed first
